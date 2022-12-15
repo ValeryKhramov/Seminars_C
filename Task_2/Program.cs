235697701e01009a6b747b3ad2f900566e10835d
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-15 21:12:04 +0300

    Fix bug in Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -2,11 +2,17 @@
 
 WriteLine("Введите номер дня недели: ");
 int a = int.Parse(ReadLine());
-
-if (a == 1) WriteLine("Понедельник");
-if (a == 2) WriteLine("Вторник");
-if (a == 3) WriteLine("Среда");
-if (a == 4) WriteLine("Четверг");
-if (a == 5) WriteLine("Пятница");
-if (a == 6) WriteLine("Суббота");
-if (a == 7) WriteLine("Воскресенье");
+if (a <= 7 && a >= 1)
+{
+    if (a == 1) WriteLine("Понедельник");
+    if (a == 2) WriteLine("Вторник");
+    if (a == 3) WriteLine("Среда");
+    if (a == 4) WriteLine("Четверг");
+    if (a == 5) WriteLine("Пятница");
+    if (a == 6) WriteLine("Суббота");
+    if (a == 7) WriteLine("Воскресенье");
+}
+else
+{
+    WriteLine("Ошибка ввода.");
+}
