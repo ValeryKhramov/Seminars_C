1f39036776772b551161abaae4d7dcc3800e76e6
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-15 21:39:04 +0300

    Solving the fourth and fifth tasks

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+int number = int.Parse(ReadLine());
+int count = - number;
+
+while (count != number + 1)
+{
+    WriteLine(count);
+    count = count + 1;
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+WriteLine("Введите трехзначное число: ");
+int number = int.Parse(ReadLine());
+
+if (number <= 999 && number >= 100)
+{
+   WriteLine($"Последняя цифра вашего числа = {number % 10}");
+}
+else
+{
+    WriteLine("Вы ввели не трехзначное число.");
+}
