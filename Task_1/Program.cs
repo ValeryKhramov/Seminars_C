dd6249d8609bbd7e237373adbf079057ca5bfe31
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-15 20:58:14 +0300

    Decision second and third task

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine());
+
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine());
+
+if (a == b*b)
+{
+    WriteLine($" Число {a} - является квадратом числа {b}");
+}
+else
+{
+    WriteLine($" Число {a} - не является квадратом числа {b}");
+}
