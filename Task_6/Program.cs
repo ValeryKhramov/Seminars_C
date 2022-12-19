38219b63e6fb2735f9722ad543b2eb3609731c75
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-19 20:56:30 +0300

    Solution Task_6 and Task_7

---
+++ Task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+
+int number = new Random().Next(100, 1000);
+int a1 = number / 100;
+int a3 = number % 10;
+WriteLine(number);
+WriteLine($"{a1}{a3}");
+
+
---
+++ Task_7/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите первое число: ");
+int number_1 = int.Parse(ReadLine());
+
+WriteLine("Введите второе число: ");
+int number_2 = int.Parse(ReadLine());
+
+WriteLine(number_1 % number_2 == 0? "Первое число кратное второму." : $"Остаток от деления равен = {number_1 % number_2}"); 
