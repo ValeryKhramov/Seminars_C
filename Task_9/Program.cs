b9a21523e10ab901adae3628834717b7ae746b6b
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-19 21:45:56 +0300

    Added modification in Task_9

--- Task_9/Program.cs
+++ Task_9/Program.cs
@@ -6,4 +6,6 @@ int number_1 = int.Parse(ReadLine());
 
 WriteLine("Введите второе число: ");
 int number_2 = int.Parse(ReadLine());
-WriteLine(number_1 * number_1 == number_2 || number_2 * number_2 == number_1 ? "Одно число являеется квадратом другого" : "Не является квадратом" );
+WriteLine(number_1 * number_1 == number_2 ? $"{number_2} является квадратом числа {number_1}" :
+     number_2 * number_2 == number_1 ? $"{number_1} является квадратом числа {number_2}" :
+     " Не одно число не является квадратом другого." );
