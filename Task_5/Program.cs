2c483564c99841bc60eced4e710a39a3c65d701f
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-19 20:24:13 +0300

    Solution Task_5

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,3 +1,17 @@
 ﻿using static System.Console;
 Clear();
 
+int number = new Random().Next(10, 100);
+int a1 = number / 10;
+int a2 = number % 10;
+WriteLine(number);
+WriteLine(a1 > a2? a1:a2);
+WriteLine(number % 2 == 0? "Even" : "Odd"); //  Тернальный оператор.
+// if (a1 > a2)
+// {
+//     WriteLine(a1);
+// }
+// else
+// {
+//     WriteLine(a2);
+// }
