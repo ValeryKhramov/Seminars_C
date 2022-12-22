dc2693fb2d783b36e18fe94cfebe78df3b49535d
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-22 21:16:53 +0300

    Added Task_11 to Task_14

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,1 +1,69 @@
-﻿
+﻿using static System.Console;
+Clear();
+
+// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+WriteLine("Введите номер четверти плоскости: ");
+int i = int.Parse(ReadLine()!);
+
+switch(i)
+{
+    case 1:
+    {
+        WriteLine(" x > 0 , y > 0");
+        break;
+    }
+     case 2:
+    {
+        WriteLine(" x < 0 , y > 0");
+        break;
+    }
+     case 3:
+    {
+        WriteLine(" x < 0 , y < 0");
+        break;
+    }
+     case 4:
+    {
+        WriteLine(" x > 0 , y < 0");
+        break;
+    }
+    default:
+    {
+        WriteLine("Ошибка ввода.");
+        break;
+    }
+}
+
+
+
+// if(i < 1 || i > 4)
+// {
+//     WriteLine("Ошибка ввода.");
+//     return;
+// }
+
+
+// if ( i == 1)
+// {
+//     WriteLine("x > 0, y > 0");
+// }
+
+// if ( i == 2)
+// {
+//     WriteLine("x < 0, y > 0");
+// }
+
+// if ( i == 3)
+// {
+//     WriteLine("x < 0, y < 0");
+// }
+
+// if ( i == 4)
+// {
+//     WriteLine("x > 0, y < 0");
+// }
+
+
+
---
+++ Task_12/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+Clear();
+
+Write("Введите значение x: ");
+if (!int.TryParse(ReadLine(), out int x))
+{
+    WriteLine("Error.Input number, not string.");
+    return;
+}
+
+Write("Введите значение y: ");
+if (!int.TryParse(ReadLine(), out int y))
+{
+    WriteLine("Error.Input number, not string.");
+    return;
+}
+
+if (x > 0)
+{
+    if (y > 0)
+    {
+        WriteLine("First quarter");
+    }
+    else
+    {
+        WriteLine("Forth quater");
+    }
+}
+if (x < 0)
+{
+    if (y > 0)
+    {
+        WriteLine("Second quarter");
+    }
+    else
+    {
+        WriteLine("Third quater");
+    }
+}
