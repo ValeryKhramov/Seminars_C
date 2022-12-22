8acc79f3ef1d9845133740920d758b32ef343756
Valery_Khamov <dr.hramoff2013@yandex.ru>
2022-12-22 21:55:49 +0300

    Added Task_14 and Task_15

--- Task_14/Program.cs
+++ Task_14/Program.cs
@@ -1,5 +1,29 @@
-﻿
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
 
 using static System.Console;
 Clear();
+WriteLine("Input coordinates first point: ");
+if (!int.TryParse(ReadLine(), out int x1))
+{
+    WriteLine("Error. Input number, not string.");
+    return;
+}
+if (!int.TryParse(ReadLine(), out int y1))
+{
+    WriteLine("Error. Input number, not string.");
+    return;
+}
+WriteLine("Input coordinates second point: ");
+if (!int.TryParse(ReadLine(), out int x2))
+{
+    WriteLine("Error. Input number, not string.");
+    return;
+}
+if (!int.TryParse(ReadLine(), out int y2))
+{
+    WriteLine("Error. Input number, not string.");
+    return;
+}
 
+WriteLine("Distance between two point is {0:f2}",Math.Sqrt(Math.Pow((x1 - x2),2) + Math.Pow((y1 - y2),2)));
---
+++ Task_15/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+
+using static System.Console;
+Clear();
+Write("Input N: ");
+if (!int.TryParse(ReadLine(), out int N))
+{
+    WriteLine("Error. Input number, not string.");
+    return;
+}
+int count = 1;
+while (count <= N)
+{
+    Write($"{Math.Pow(count, 2 )} ");
+    count++;
+}
