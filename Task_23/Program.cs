de100670e142f49aa885ed70b33fc842cbcb5afe
Valery_Khamov <dr.hramoff2013@yandex.ru>
2023-01-16 20:59:48 +0300

    Start seminar number 5

---
+++ Task_23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 31: Задайте массив из 12 элементов, 
+// заполненный случайными числами из промежутка [-9, 9].
+//  Найдите сумму отрицательных и положительных элементов массива.
+
+//сделать метод кторый возрщает int
+
+
+using static System.Console;
+Clear();
+
+Write("Input size array, minimal value array and maximum value array; ");
+string[] parameters = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntires);
